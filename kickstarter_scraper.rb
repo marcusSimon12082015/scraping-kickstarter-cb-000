1984c12ca07aed06069f12c4f6e9872fd090ab1d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-05 15:18:20 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,5 +5,5 @@ def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-
+  kickstarter.css("li.project.grid_4").first
 end

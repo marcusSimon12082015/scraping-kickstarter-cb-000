549b95403a558b9003cf01db65ff6b3187f2a29f
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-11-05 15:23:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,5 @@ def create_project_hash
   #image link: project.css("div.project-thumbnail a img").attribute("src").value
   #description: project.css("p.bbcard_blurb").text
   #location: project.css("li span.location-name").text
-
+  #funded: project.css("ul.project-stats li.first.funded strong").text
 end
